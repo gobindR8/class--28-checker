d35f77a7d0cc695b17303e7ec9a4884b931ec355
gobindR8 <74371640+gobindR8@users.noreply.github.com>
2021-07-24 21:35:06 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,35 @@
+class launcher{
+constructor(bodyA,pointB){
+    var options= {
+    bodyA:bodyA,
+    pointB:pointB,
+    siffness:0.04,
+    length:10
+    }
+    this.pointB=pointB
+    this.launcher=Constraint.create(options)
+    World.add(world,this.launcher)
+}
+
+    fly(){
+        this.launcher.body=null
+    }
+
+display(){
+if(this.launcher.bodyA){
+var pointA=this.launcher.bodyA.position
+var pointB=this.pointB
+strokeWeight(4)
+line(pointA.x,pointA.y,pointB.x,pointB.y)
+}
+}
+
+
+
+
+
+}
+
+
+
+
